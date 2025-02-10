efb9b02a30ae65468430f288a2d4a041bfbb49b1
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-02-09 22:29:37 -0500

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,9 @@
+#Write a Python program to calculate the length of a string
+#reference:https://www.w3resource.com/python-exercises/string/
+
+def string_length(string):
+    my_list=list(string)
+    result=len(my_list)
+    return result
+
+print(string_length("BiswajitBall"))
