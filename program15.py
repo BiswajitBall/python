38c3fe55daf9460cc2dac19699e558ae6589d29a
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-08-05 13:14:32 -0400

    Create program15.py

---
+++ program15.py
@@ -0,0 +1,22 @@
+#write a class employee in python
+class Employee:
+    # class variables
+    company_name = 'ABC Company'
+
+    # constructor to initialize the object
+    def __init__(self, name, salary):
+        # instance variables
+        self.name = name
+        self.salary = salary
+
+    # instance method
+    def show(self):
+        print('Employee:', self.name, self.salary, self.company_name)
+
+# create first object
+emp1 = Employee("Makaia", 12000)
+emp1.show()
+
+# create second object
+emp2 = Employee("Antony", 10000)
+emp2.show()
